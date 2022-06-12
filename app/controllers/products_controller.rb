0835162af05b1b529650400ef238f745ad6724c1
Viktor Tagil <viktor.tagil@gmail.com>
2022-06-12 21:46:59 +0200

    new and create actions added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,27 @@
 class ProductsController < ApplicationController
+  before_action :find_warehouse
   def new
-    @products = Product.new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.warehouse = @warehouse
+
+    if @product.save
+      redirect_to warehouse_path(@warehouse)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :quantity)
+  end
+
+  def find_warehouse
+    @warehouse = Warehouse.find(params[:warehouse_id])
   end
 end

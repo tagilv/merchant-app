79b2351b85efb9e2626bbe6231f42ba8ec15a420
Viktor Tagil <viktor.tagil@gmail.com>
2022-06-12 16:51:05 +0200

    all crud actions for warehouses

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :warehouses, only: [:new, :create, :index, :show] do
+  resources :warehouses do
     resources :products, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+# , only: [:new, :create, :index, :show :]

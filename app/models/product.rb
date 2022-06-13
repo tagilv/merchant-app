73d5b327b6017ffee47a93663d648c97c5a6ff2b
Viktor Tagil <viktor.tagil@gmail.com>
2022-06-13 22:30:59 +0200

    removed nesting

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
   belongs_to :warehouse
-  validates :name, :price, :description, :quantity, presence: true
+  validates :name, :price, :description, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  resources :warehouses do
-    resources :products, only: [ :new, :create, :index ]
-  end
-  resources :products, only: [ :show, :edit, :update, :destroy ]
+  resources :warehouses
+  resources :products
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
+# Rails.application.routes.draw do
+#   resources :products, only: [ :new, :create, :index, :show, :edit, :update, :destroy ]
+
+#   resources :warehouses do
+#     resources :products, only: [ :new, :create, :index ]
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+#   end
+# end
